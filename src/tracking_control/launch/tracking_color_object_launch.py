cefabb9b106d7eb02e9f0a3325d91ed34903bced
vesta2677 <theasteinba@gmail.com>
2025-03-21 11:58:46 -0400

    Update tracking_color_object_launch.py

--- src/tracking_control/launch/tracking_color_object_launch.py
+++ src/tracking_control/launch/tracking_color_object_launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
         executable='color_goal_detection',
         name='color_goal_detection_node',
         parameters=[
-            {'color_low': [100, 50, 150]},{'color_high': [130, 255, 255]}, {'object_size_min':200}
+            {'color_low': [0, 50, 150]},{'color_high': [130, 255, 255]}, {'object_size_min':200}
         ],
         output="screen"
     )
