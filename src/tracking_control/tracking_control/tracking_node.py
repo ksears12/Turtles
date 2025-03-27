f2ba8c922339723ce7bb5c6594711ee6fc9f0669
vesta2677 <theasteinba@gmail.com>
2025-03-27 14:32:27 -0400

    3567

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -208,8 +208,8 @@ class TrackingNode(Node):
             current_obs_pose, current_goal_pose = self.get_current_poses()
         except:
             current_obs_pose, current_goal_pose = np.ones(3),np.ones(3)
-        self.get_logger().info('Transform Goal: {}'.format(current_obs_pose))
-        self.get_logger().info('Transform Obs: {}'.format(current_goal_pose))
+        # self.get_logger().info('Transform Goal: {}'.format(current_obs_pose))
+        # self.get_logger().info('Transform Obs: {}'.format(current_goal_pose))
         k_rep = 10
         k_att = 1
         k_turn = 1
@@ -238,8 +238,8 @@ class TrackingNode(Node):
         u_z_att = k_turn*current_goal_pose[1]
 
         cmd_vel = Twist()
-        cmd_vel.linear.x = u_x_att
-        cmd_vel.linear.y = 0.0
+        cmd_vel.linear.x = u_x_att+u_x_rep
+        cmd_vel.linear.y = u_y_rep
         cmd_vel.angular.z = u_z_att
         # self.get_logger().info('Transform Error: {}'.format(cmd_vel))
         return cmd_vel
