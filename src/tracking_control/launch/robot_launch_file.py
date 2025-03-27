8ec1a50be8d12935596b28cb6cc2b0637a015793
vesta2677 <theasteinba@gmail.com>
2025-03-27 12:22:03 -0400

    new launch file

--- src/tracking_control/launch/robot_launch_file.py
+++ src/tracking_control/launch/robot_launch_file.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node 
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 import os
 from ament_index_python.packages import get_package_share_directory
 
@@ -10,6 +11,8 @@ def generate_launch_description():
     tracking_pkg = 'tracking_control'
     obj_detection_package_path = get_package_share_directory(object_detection_pkg)
     tracking_package_path = get_package_share_directory(tracking_pkg)
+    cam_package_path = get_package_share_directory('astra_camera')
+    yahboomcar_package_path = get_package_share_directory('yahboomcar_bringup')
     
     obj_detection_node = Node(
         package=object_detection_pkg,
@@ -37,9 +40,27 @@ def generate_launch_description():
         name='tracking_node',
         output="screen"
     )
+
+    motor_control_node = Node(
+        package=yahboomcar_package_path,
+        executable='Mcnamu_driver_X3',
+        name='motor_control_node',
+        output="screen"
+    )
+
+    astra_camera_launch = IncludeLaunchDescription(XMLLaunchDescriptionSource(
+        [os.path.join(cam_package_path, 'launch'),
+         '/astra_pro.launch.xml'])
+    )
+    yahboomcar_brinup_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        [os.path.join(yahboomcar_package_path, 'launch'),
+         '/yahboomcar_bringup_X3_launch.py'])
+    )
     
     return LaunchDescription([
         obj_detection_node,
         goal_detection_node,
-        tracking_control_node
+        tracking_control_node,
+        astra_camera_launch,
+        yahboomcar_brinup_launch
     ])
--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -66,8 +66,8 @@ class TrackingNode(Node):
         self.get_logger().info('Tracking Node Started')
         
         # Current object pose
-        self.obs_pose = 5
-        self.goal_pose = 5
+        self.obs_pose = np.array([0,0,0])
+        self.goal_pose = np.array([0,0,0])
         
         # ROS parameters
         self.declare_parameter('world_frame_id', 'odom')
