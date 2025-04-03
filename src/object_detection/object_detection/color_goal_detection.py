4c54af0999e00c4d5c27fde3586bb40ed7ad7fe4
vesta2677 <theasteinba@gmail.com>
2025-04-03 15:11:07 -0400

    zero value not qwhite

--- src/object_detection/object_detection/color_goal_detection.py
+++ src/object_detection/object_detection/color_goal_detection.py
@@ -164,7 +164,7 @@ class ColorObjDetectionNode(Node):
                 for k in range(index1[0].size):
                     index1a = index1[0][k]
                     index1b = index1[1][k]
-                    image[index1a,index1b]=image[index1a,index1b]/image[index1a,index1b]*255
+                    image[index1a,index1b]=image[index1[0][0],index1[1][0]]
                     # image[index1a,index1b]+=255
 
                 plt.imshow(image)
